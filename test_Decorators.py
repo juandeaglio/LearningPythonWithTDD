74db83b88a0976e0e84f27f9d0c89f77a9757b2f
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-03 17:42:01 -0400

    Initial attempt at decorator (w/o @ annotation).

--- test_Decorators.py
+++ test_Decorators.py
@@ -3,12 +3,23 @@ import unittest
 from tempfile import TemporaryFile
 
 
+def say_goodbye():
+    return "Goodbye"
+
 class Decorators(unittest.TestCase):
     def test_unDecoratedFunction(self):
-        self.assertTrue("hello" == self.greet())
+        self.assertTrue("Hello" == self.greet())
 
     def greet(self):
-        return "hello"
+        return "Hello"
+
+    def test_decoratedFunction(self):
+        self.assertTrue("""Hello
+Goodbye""" == self.conversation(self.greet))
+
+
+    def conversation(self, introduction):
+        return introduction() + "\n" + say_goodbye()
 
 
 
