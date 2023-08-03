a6388da83edff119a89ce091d451b436a9c9f41b
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-08-03 17:44:49 -0400

    Styling changes

--- test_Decorators.py
+++ test_Decorators.py
@@ -1,10 +1,9 @@
-import os
 import unittest
-from tempfile import TemporaryFile
 
 
 def say_goodbye():
     return "Goodbye"
+
 
 class Decorators(unittest.TestCase):
     def test_unDecoratedFunction(self):
@@ -17,10 +16,8 @@ class Decorators(unittest.TestCase):
         self.assertTrue("""Hello
 Goodbye""" == self.conversation(self.greet))
 
-
     def conversation(self, introduction):
         return introduction() + "\n" + say_goodbye()
-
 
 
 if __name__ == '__main__':
